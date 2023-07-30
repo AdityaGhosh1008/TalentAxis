42da4f184fadc8c83bcbcb6e82f65cec116461cf
AdityGhosh1008 <aditya20276@iiitd.ac.in>
2023-07-30 12:05:35 +0530

    change5

---
+++ mail.js
@@ -0,0 +1,35 @@
+function hidetext1() {
+    const redirectConfirmation = confirm("Do you want apply for Web Developer at ABC corp?");
+
+    if (redirectConfirmation) {
+        window.location.href = "https://www.google.com";
+    }
+}
+function hidetext2() {
+    const redirectConfirmation = confirm("Do you want apply for Graphic Designer at XYZ studios?");
+
+    if (redirectConfirmation) {
+        window.location.href = "https://www.google.com";
+    }
+}
+function hidetext3() {
+    const redirectConfirmation = confirm("Do you want apply for Sales at PKC inc?");
+
+    if (redirectConfirmation) {
+        window.location.href = "https://www.google.com";
+    }
+}
+function hidetext4() {
+    const redirectConfirmation = confirm("Do you want apply for VP of Design at Design ventures?");
+
+    if (redirectConfirmation) {
+        window.location.href = "https://www.google.com";
+    }
+}
+function hidetext5() {
+    const redirectConfirmation = confirm("Do you want apply for HR manager at JCC Cement?");
+
+    if (redirectConfirmation) {
+        window.location.href = "https://www.google.com";
+    }
+}
